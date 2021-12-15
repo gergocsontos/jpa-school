3611dd58d6bf2ad0b0069d59f5d09dc5844ed1cf
Gergő Csontos <gergo.csontos.123@gmail.com>
2021-12-15 19:54:33 +0100

    add spring project

---
+++ src/test/java/com/codecool/jpaschool/JpaSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.jpaschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaSchoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
